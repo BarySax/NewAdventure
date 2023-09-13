66a2996823fdf7cb4e558acc7a9747914f729762
Nicolas Prigge <priggen@educ.college-montreal.qc.ca>
2023-09-13 10:18:46 -0400

    correction dans le main.py

--- nom.py
+++ nom.py
@@ -1,4 +1,4 @@
-
+import time
 
 def demande_du_nom():
     #variable de programe
@@ -6,6 +6,7 @@ def demande_du_nom():
 
     #introduction
     print("vous etiez parti explorez le systeme planetaire de kepler-186 et un astreoide a percuter le vessaux et vous a fait tomber sur la planete kepler-186b")
+    time.sleep(1)
 
     while True:
         nom = input("vous ne vous rappelr pas de vorte nom donc comment voulez vous vous appeler:")
@@ -14,3 +15,5 @@ def demande_du_nom():
             print("svp entrez un nom")
         else:
             break
+
+demande_du_nom()
