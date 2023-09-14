5b1a7909853e2da5a39d10ebf673aedfe152c3b8
steiyve <94018684+steiyve@users.noreply.github.com>
2023-09-14 10:01:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import time
+import player
+import enemy
 
-def demande_du_nom():
-    #variable de programe
-    nom = ""
+#initialization de classe
+player = player.Player()
+enemy  = enemy.Enemy()
 
-    #introduction
-    print("vous etiez parti explorez le systeme planetaire de kepler-186 et un astreoide a percuter le vessaux et vous a fait tomber sur la planete kepler-186b")
-    time.sleep(1)
-
-    while True:
-        nom = input("vous ne vous rappelr pas de vorte nom donc comment voulez vous vous appeler:")
-
-        if nom == "":
-            print("svp entrez un nom")
-        else:
-            break
-
-demande_du_nom()
+player.fight()
+player.printInfo()
