42c8dca211e570602fb03b9899a13deddc3902a7
BarySax <144921224+BarySax@users.noreply.github.com>
2023-09-28 13:32:51 -0400

    Choose et pas chose

--- enemy.py
+++ enemy.py
@@ -1,13 +1,9 @@
-##W##########################################################A CHANGAERAREWRERERAERAEEARRAERAERAEWRAWWWRF
 import random
 import time
 class Enemy:
    def __init__(self):
       bloup = 30
       self.hp = 100
-      
-      
-
-   def chose_enemy(self):
+   def choose_enemy(self):
       self.type = ["joe", "bob"]
       self.nom = random.choice(self.type)
